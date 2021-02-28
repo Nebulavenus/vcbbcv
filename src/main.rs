d2ff5632f704ea9037e1f8bbb36c1edb1ce01141
Nebula Venus <nebulavenus@tuta.io>
2021-02-28 16:11:12 +0100

    Draw & update functions

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
 use glsp::prelude::*;
 use macroquad::prelude::*;
 
+fn clear_background_wrapper(color: (u8, u8, u8, u8)) -> GResult<()> {
+    let c = color_u8!(color.0, color.1, color.2, color.3);
+    clear_background(c);
+    Ok(())
+}
+
 fn draw_circle_wrapper(x: f32, y: f32, r: f32, color: (u8, u8, u8, u8)) -> GResult<()> {
     let c = color_u8!(color.0, color.1, color.2, color.3);
     draw_circle(x, y, r, c);
+    Ok(())
+}
+
+fn draw_text_wrapper(
+    text: &str,
+    x: f32,
+    y: f32,
+    font_size: f32,
+    color: (u8, u8, u8, u8),
+) -> GResult<()> {
+    let c = color_u8!(color.0, color.1, color.2, color.3);
+    draw_text(text, x, y, font_size, c);
     Ok(())
 }
 
@@ -16,7 +34,9 @@ async fn main() {
         glsp::load("scripts/main.glsp")?;
 
         // Bind functions
+        glsp::bind_rfn("clear-background", &clear_background_wrapper)?;
         glsp::bind_rfn("draw-circle", &draw_circle_wrapper)?;
+        glsp::bind_rfn("draw-text", &draw_text_wrapper)?;
 
         Ok(())
     });
@@ -36,15 +56,29 @@ async fn main() {
                 }
             };
 
-            clear_background(RED);
-
             let dt = get_frame_time();
             let _: Val = match glsp::call(&update, (dt,)) {
                 Ok(val) => val,
                 Err(err) => return Err(err),
             };
 
-            draw_circle(50., 50., 10., BLUE);
+            // Call scripting draw
+            let draw: Root<GFn> = match glsp::global::<_, Val>("engine:draw") {
+                Ok(Val::GFn(draw)) => draw,
+                Ok(val) => {
+                    let msg = format!("invalid engine:draw value {}", val);
+                    return Err(GError::from_str(msg.as_str()));
+                }
+                Err(err) => {
+                    let msg = format!("engine:draw is not defined {}", err);
+                    return Err(GError::from_str(msg.as_str()));
+                }
+            };
+
+            let _: Val = match glsp::call(&draw, ()) {
+                Ok(val) => val,
+                Err(err) => return Err(err),
+            };
 
             glsp::gc();
 
